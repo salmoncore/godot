427d6e28f356ed146264cea0b88739648304a217
Arthur Upfield <arthurupfield@outlook.com>
2023-07-11 17:57:10 -0400

    Testing without collision

--- Dogged/node_2d.gd
+++ Dogged/node_2d.gd
@@ -1,31 +1,32 @@
 extends Node2D
 
-var speedy = -150
+var selectNode
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	
+	if selectNode != null:
+		if Input.is_action_pressed("key_right"):
+			$Torso/Neck.rotation_degrees += 1
+		if Input.is_action_pressed("key_left"):
+			selectNode.rotation_degrees -= 1
 	
 	pass
 
 
-	#var speedy = -150
-#	$Torso.rotation_degrees += delta * speedy
-#	$Torso/Neck.rotation_degrees += delta * speedy
-#	$Torso/Neck/Head.rotation_degrees += delta * speedy
-#	$Torso/Neck/Head/Ear.rotation_degrees += delta * speedy
-#	$Torso/LegFrontL.rotation_degrees += delta * speedy
-#	$Torso/LegFrontR.rotation_degrees += delta * speedy
-#	$Torso/Back.rotation_degrees += delta * speedy
-#	$Torso/Back/LegBackL.rotation_degrees += delta * speedy
-#	$Torso/Back/LegBackR.rotation_degrees += delta * speedy
-#	$Torso/Back/TailSection1.rotation_degrees += delta * speedy
-#	$Torso/Back/TailSection1/TailSection2.rotation_degrees += delta * speedy
-#	$Torso/Back/TailSection1/TailSection2/TailSection3.rotation_degrees += delta * speedy
+func _on_torso_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		selectNode = get_node("Torso")
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		selectNode = null
+
+func _on_neck_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		selectNode = get_node("Torso/Neck")
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		selectNode = null
