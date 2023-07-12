62f067a34cd304251c554895fa2aeb30a90a5eac
Arthur Upfield <arthurupfield@outlook.com>
2023-07-12 19:36:33 -0400

    Game Complete
    Goin gold :)

--- Dogged/node_2d.gd
+++ Dogged/node_2d.gd
@@ -4,6 +4,7 @@ var selectNode
 var selectSprite
 var poseNode
 var poseSprite
+var tmpNode
 var partArray = ["Torso", "Torso/Neck", "Torso/Neck/Head", "Torso/Neck/Head/Ear", "Torso/Back", "Torso/Back/LegBackR",
                  "Torso/Back/LegBackL", "Torso/Back/TailSection1", "Torso/Back/TailSection1/TailSection2", 
                  "Torso/Back/TailSection1/TailSection2/TailSection3", "Torso/LegFrontR", "Torso/LegFrontL"]
@@ -13,13 +14,11 @@ var poseArray = ["PoseTorso", "PoseTorso/Neck", "PoseTorso/Neck/Head", "PoseTors
 var partIndex = 0
 var posePositions = [0,0,0,0,0,0,0,0,0,0,0,0]
 var jointScores = [0,0,0,0,0,0,0,0,0,0,0,0]
+var lbScores = []
 var scoreTotal = 0
 var colorArray = [0,0,0]
 var colorHighlightArray = [0,0,0]
 var contestantNum = 0
-var timerSecs = 90
-
-# 0 for intro, 1 for randomizer, 2 for posing, 3 for player control, 4 for score
 var gameState = 0
 
 func _ready():
@@ -27,56 +26,79 @@ func _ready():
     $TextContainer/LabelInfo.text = "Press ENTER/CONFIRM to begin!"
 
 func _process(delta):
-    
-    if gameState == 0:
-        if Input.is_action_just_pressed("key_confirm"):
-            contestantNum += 1
-            gameState = 1
-    
-    if gameState == 1:
-        $TextContainer/LabelInfo.text = ":^)"
-        resetDogs()
-        randomizer()
-    
-    if gameState == 2:
-        $TextContainer/LabelInfo.text = "WATCH THE EXAMPLE DOG"
-        $TextContainer/LabelTime.text = "←←←←"
-        dogPoser()
-    
-    if gameState == 3:
-        $TextContainer/LabelInfo.text = "now do that! :^)"
-        $TextContainer/LabelTime.text = "timer"
-        playerControl()
-        if Input.is_action_just_pressed("key_confirm"):
-            gameState = 4
             
-    if gameState == 4:
-        dogGrader()
-        $TextContainer/LabelInfo.text = String("Contestant %d's score:" % contestantNum)
-        $TextContainer/LabelTime.text = String("%d" % scoreTotal)
-        if Input.is_action_just_pressed("key_confirm"):
-            colorRandomizer()
-            resetGrade()
-            gameState = 0
+    match (gameState):
+        0:
+            $TextContainer/LabelInfo.text = "Press ENTER/CONFIRM to begin!"
+            if Input.is_action_just_pressed("key_confirm"):
+                contestantNum += 1
+                gameState += 1
+        1:
+            $TextContainer/LabelInfo.text = ":^)"
+            poseRandomizer()
+            gameState += 1
+        2:
+            $TextContainer/LabelInfo.text = "WATCH THE EXAMPLE DOG"
+            $TextContainer/LabelTime.text = "←←←←"
+            dogPoser()
+        3:
+            $Timer.start(61)
+            gameState += 1
+        4:
+            $TextContainer/LabelInfo.text = "now do that! :^)"
+            $TextContainer/LabelTime.text = String("%d" % $Timer.time_left)
+            playerControl()
+            if Input.is_action_just_pressed("key_confirm"):
+                gameState += 1
+        5:
+            dogGrader()
+            gameState += 1
+        6:
+            $TextContainer/LabelInfo.text = String("Contestant %d's score:" % contestantNum)
+            $TextContainer/LabelTime.text = String("%d" % scoreTotal)
+            lbScores = appendScore(contestantNum, scoreTotal)
+            writeLeaderBoard()
+            gameState += 1
+        7: 
+            if Input.is_action_just_pressed("key_confirm"):
+                colorRandomizer()
+                resetDogs()
+                resetGrade()
+                gameState = 0
+                
+func writeLeaderBoard():
+    var tmpNode
+    for i in range(lbScores.size()):
+        tmpNode = get_node("TextContainer/LeaderBoard/LBContestant%d" % i)
+        tmpNode.text = "Contestant"
+        tmpNode = get_node("TextContainer/LeaderBoard/LBColon%d" % i)
+        tmpNode.text = ":"
+        tmpNode = get_node("TextContainer/LeaderBoard/LBID%d" % i)
+        tmpNode.text = String("%d" % lbScores[i][0])
+        tmpNode = get_node("TextContainer/LeaderBoard/LBScore%d" % i)
+        tmpNode.text = String("%d" % lbScores[i][1])
+                
+func appendScore(a, b):
+    var buffer = lbScores
+    # push score to array
+    buffer.push_back([a, b])
+    # sort score low to high
+    buffer.sort_custom(sortScores)
+    # if more than 8 array values, pop highest value (pop back)
+    if buffer.size() > 8:
+        buffer.pop_back()
+    return buffer
     
-func timerSet():
-    # Reset the timer to 0
-    pass
-
-func timerStart():
-    # Start timer, update screen
-    pass
-
-func randomizer():
-    poseRandomizer()
-    gameState = 2
+func sortScores(a, b): # Ascending Order
+    if a[1] < b[1]:
+        return true
+    return false
 
 func poseRandomizer():
     randomize()
     
     for i in range(12):
         poseNode = get_node(poseArray[i])
-        # Randomly rotates the dog's joints
         posePositions[i] = randi() % 360
 
 func colorRandomizer():
@@ -115,8 +137,9 @@ func dogPoser():
             poseNode.rotation_degrees = 0
         if poseNode.rotation_degrees >= posePositions[i]:
             poseSet += 1
-        if poseSet == 12:
-            gameState = 3
+    
+    if poseSet == 12:
+        gameState += 1
 
 func dogGrader():
     for i in range(12):
@@ -129,7 +152,6 @@ func resetGrade():
     for i in range(12):
         jointScores[i] = 0
         scoreTotal = 0
-    print("Score Reset")
 
 func angleDist(angle1, angle2):
     var diff = abs(angle1 - angle2) % 360
@@ -161,9 +183,7 @@ func playerControl():
         selectSprite = get_node(partArray[partIndex] + "/Sprite2D")
         selectSprite.modulate = Color(colorHighlightArray[0],colorHighlightArray[1],colorHighlightArray[2])
     
-    # Handle input for part selection
-    
-    
+    # Handle input for part selection    
     if Input.is_action_just_pressed("key_up"):
         selectSprite.modulate = Color(colorArray[0],colorArray[1],colorArray[2])
         if partIndex == 11:
@@ -183,6 +203,5 @@ func playerControl():
         selectSprite = get_node(partArray[partIndex] + "/Sprite2D")
         selectSprite.modulate = Color(colorHighlightArray[0],colorHighlightArray[1],colorHighlightArray[2])
 
-## Todo
-# - clock
-# - leaderboard
+func _on_timer_timeout():
+    gameState += 1
