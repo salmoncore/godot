0bb073cd510597832401b34b494ce54cd817435d
Arthur Upfield <arthurupfield@outlook.com>
2023-07-29 21:34:30 -0400

    new camera logic

--- Po-Go/root.gd
+++ Po-Go/root.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 var player
-var maxVelocity = 0
+var velocity = 0
 
-const ZOOM_IN_MAX = Vector2(1, 1)
 const ZOOM_OUT_MAX = Vector2(.3, .3)
-const ZOOM_STANDARD = Vector2(.7, .7)
-var zoom = Vector2(.8, .8)
+const ZOOM_IN_MAX = Vector2(.7, .7)
+const ZOOM_INCREMENT = Vector2(.001, .001)
+var zoom = Vector2(.7, .7)
 var mod = 0
 var maxMod = 0
 
@@ -18,37 +18,29 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Camera.offset = player.position
-	
-	if abs(player.velocity.x) > abs(player.velocity.y):
-		maxVelocity = abs(player.velocity.x)
-	else:
-		maxVelocity = abs(player.velocity.y)
 		
-#	print(maxVelocity)
-	
-	if maxVelocity > 500:
-		mod = abs(maxVelocity - 500) / 100000
-		
-		if mod > maxMod:
-			maxMod = mod
-			print(maxMod)
-		
-		zoom.x = zoom.x - maxMod
-		zoom.y = zoom.y - maxMod
-		
-		if zoom > ZOOM_IN_MAX:
-			zoom = ZOOM_IN_MAX
-		if zoom < ZOOM_OUT_MAX:
-			zoom = ZOOM_OUT_MAX
-			
+	if playerVelocity() > 500:
+		zoomOut()
 	elif player.is_on_floor:
-		mod = abs(500 - maxVelocity) / 10000
-		zoom.x = zoom.x + mod
-		zoom.y = zoom.y + mod
-		
-		if zoom > ZOOM_IN_MAX:
-			zoom = ZOOM_IN_MAX
-		if zoom < ZOOM_OUT_MAX:
-			zoom = ZOOM_OUT_MAX
-		
+		zoomIn()
+	
+func zoomIn():
+	zoom = zoom + ZOOM_INCREMENT
+	zoomSet()
+	
+func zoomOut():
+	zoom = zoom - ZOOM_INCREMENT
+	zoomSet()
+	
+func playerVelocity():
+	if abs(player.velocity.x) > abs(player.velocity.y):
+		return abs(player.velocity.x)
+	else:
+		return abs(player.velocity.y)
+	
+func zoomSet():
+	if zoom > ZOOM_IN_MAX:
+		zoom = ZOOM_IN_MAX
+	if zoom < ZOOM_OUT_MAX:
+		zoom = ZOOM_OUT_MAX
 	$Camera.set_zoom(zoom)
