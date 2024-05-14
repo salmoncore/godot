5e11e4b02739f428673c3ddc1f62bd2fd11b06eb
Arthur Upfield <arthurupfield@outlook.com>
2024-05-14 17:25:30 -0400

    Scorekeeping

--- Primer/scripts/game_manager.gd
+++ Primer/scripts/game_manager.gd
@@ -1,7 +1,11 @@
 extends Node
 
 var score = 0
+@onready var score_label = $ScoreLabel
 
 func add_point():
 	score += 1
-	print(score)
+	if score == 1:
+		score_label.text = "collected: " + str(score) + " coin!!!"
+	else:
+		score_label.text = "collected: " + str(score) + " coins!!!"
